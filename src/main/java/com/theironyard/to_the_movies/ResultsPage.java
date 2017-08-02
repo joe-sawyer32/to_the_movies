84e41b6c0d1f3824f162432888cb346399234c93
Joe <joe-sawyer32@users.noreply.github.com>
2017-08-02 10:59:42 -0500

    ResultsPage javabean created

---
+++ src/main/java/com/theironyard/to_the_movies/ResultsPage.java
@@ -0,0 +1,18 @@
+package com.theironyard.to_the_movies;
+
+import java.util.List;
+
+public class ResultsPage {
+    private List<Movie> results;
+
+    public ResultsPage() {
+    }
+
+    public List<Movie> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Movie> results) {
+        this.results = results;
+    }
+}
