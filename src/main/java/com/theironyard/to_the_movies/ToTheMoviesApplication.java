b901afc637c849309f6119de2f75317a4ec106ac
Joe <joe-sawyer32@users.noreply.github.com>
2017-08-02 10:55:11 -0500

    Initial commit

---
+++ src/main/java/com/theironyard/to_the_movies/ToTheMoviesApplication.java
@@ -0,0 +1,12 @@
+package com.theironyard.to_the_movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToTheMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToTheMoviesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/theironyard/to_the_movies/ToTheMoviesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.theironyard.to_the_movies;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ToTheMoviesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
