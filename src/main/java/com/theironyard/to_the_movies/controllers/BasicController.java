c9255644ee180bec4fc5ab40b1dc8bbe03fe620a
Joe <joe-sawyer32@users.noreply.github.com>
2017-08-02 16:50:31 -0500

    Now playing page displaying pictures and titles for the picky friend

--- src/main/java/com/theironyard/to_the_movies/controllers/BasicController.java
+++ src/main/java/com/theironyard/to_the_movies/controllers/BasicController.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class BasicController {
     private static final String API_TOKEN = "be2a38521a7859c95e2d73c48786e4bb";
     private static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=" + API_TOKEN;
+    private static RestTemplate restTemplate = new RestTemplate();
 
     @RequestMapping(path = "/home", method = RequestMethod.GET)
     public String home() {
@@ -27,12 +29,16 @@ public class BasicController {
     }
 
     @RequestMapping(path = "/medium-popular-long-name", method = RequestMethod.GET)
-    public String mediumPopularLongName() {
+    public String mediumPopularLongName(Model model) {
+        model.addAttribute("movies", getMovies(NOW_PLAYING_URL)
+                .stream()
+                .filter(movie -> movie.getTitle().length() > 9)
+                .filter(movie -> movie.getPopularity() > 29 && movie.getPopularity() < 81)
+                .collect(Collectors.toList()));
         return "medium-popular-long-name";
     }
 
     public static List<Movie> getMovies(String route) {
-        RestTemplate restTemplate = new RestTemplate();
         ResultsPage resultsPage = restTemplate.getForObject(route, ResultsPage.class);
         return resultsPage.getResults();
     }
