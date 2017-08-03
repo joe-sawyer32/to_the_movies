697af8cbb6311fa307c96fea30c5cc15ba5932c9
Joe <joe-sawyer32@users.noreply.github.com>
2017-08-02 21:36:18 -0500

    Extras completed somewhat functionally

--- src/main/java/com/theironyard/to_the_movies/controllers/BasicController.java
+++ src/main/java/com/theironyard/to_the_movies/controllers/BasicController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 @Controller
@@ -16,6 +19,7 @@ public class BasicController {
     private static final String API_TOKEN = "be2a38521a7859c95e2d73c48786e4bb";
     private static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=" + API_TOKEN;
     private static RestTemplate restTemplate = new RestTemplate();
+    private static Random random = new Random();
 
     @RequestMapping(path = "/home", method = RequestMethod.GET)
     public String home() {
@@ -36,6 +40,25 @@ public class BasicController {
                 .filter(movie -> movie.getPopularity() > 29 && movie.getPopularity() < 81)
                 .collect(Collectors.toList()));
         return "medium-popular-long-name";
+    }
+
+    @RequestMapping(path = "/overview-mashup", method = RequestMethod.GET)
+    public String overviewMashup(Model model) {
+        String mashupString = "";
+        List<String> randomSentences = new ArrayList<>();
+        getMovies(NOW_PLAYING_URL).stream()
+                .limit(5)
+                .forEach((movie -> {
+                    randomSentences.add(movie.getOverview()
+                            .split("\\.")[random.nextInt(
+                                                movie.getOverview().split("\\.").length)]);
+                }));
+        Collections.sort(randomSentences, (e1, e2) -> random.nextInt(3) - 1);
+        for (String sentence : randomSentences) {
+            mashupString += sentence + ". ";
+        }
+        model.addAttribute("mashupString", mashupString);
+        return "overview-mashup";
     }
 
     public static List<Movie> getMovies(String route) {
